026d0984820796a3e2816601e5c5fdad1d5add24
Anthony Doud <anthonydoud97@gmail.com>
2024-11-24 17:08:06 -0600

    another xcode build try.

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,10 +6,6 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-# Debug: Print available environment variables (secrets will be redacted)
-echo "Available environment variables:"
-env
-
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b 3.22.3 $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
@@ -20,16 +16,14 @@ flutter precache --ios
 # Install Flutter dependencies
 flutter pub get
 
+# Build the Flutter app with the extra arguments from environment variable
+flutter build ios --release --no-codesign $FLUTTER_EXTRA_ARGS
+
 # Install CocoaPods using Homebrew.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
 
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.
-
-# Note: When Xcode Cloud runs the actual build, it will use the build settings
-# configured in Xcode, which should include the --dart-define arguments:
-# --dart-define=STRAVA_CLIENT_ID=${STRAVA_CLIENT_ID}
-# --dart-define=STRAVA_CLIENT_SECRET=${STRAVA_CLIENT_SECRET}
 
 exit 0
